9d6233b10b1721c2aa067b79780bf460b05debb3
yoshinGO <kamemygenki0124@gmail.com>
2018-11-26 17:47:16 +0900

    chapter1 done

--- chapter01/n06.py
+++ chapter01/n06.py
@@ -1,5 +1,5 @@
 """
-"paraparaparadise"と"paragraph"に含まれる文字bi-gramの集合を，それぞれ, XとYとして求め，
+'paraparaparadise'と'paragraph'に含まれる文字bi-gramの集合を，それぞれ, XとYとして求め，
 XとYの和集合，積集合，差集合を求めよ．
 さらに，'se'というbi-gramがXおよびYに含まれるかどうかを調べよ．
 """
--- chapter01/n09.py
+++ chapter01/n09.py
@@ -2,26 +2,12 @@
 スペースで区切られた単語列に対して，各単語の先頭と末尾の文字は残し，
 それ以外の文字の順序をランダムに並び替えるプログラムを作成せよ．
 ただし，長さが４以下の単語は並び替えないこととする．
-適当な英語の文（例えば"I couldn't believe that I could actually understand
-what I was reading : the phenomenal power of the human mind ."）を与え，
+適当な英語の文（例えば'I couldn't believe that I could actually understand
+what I was reading : the phenomenal power of the human mind .'）を与え，
 その実行結果を確認せよ．
 """
 
 import random
-
-"""
-# 文字の順番をバラバラにする
-def element_shuffle(ele):
-    ele_list = list(ele)  # 渡された単語が一文字ずつリストに格納する
-    tmp = list(ele)  # shuffle後に元のリストの情報が必要になるため
-    random.shuffle(ele_list)  # ele_listは要素がバラバラになった
-    orign_head_index = ele_list.index(tmp[0])  # 元のele_listの先頭の文字がshuffle後にどこにいるか
-    orign_end_index = ele_list.index(tmp[len(ele_list)-1])  # 元のele_listの末尾の文字がshuffle後にどこにいるか
-    ele_list[0], ele_list[orign_head_index] = ele_list[orign_head_index], ele_list[0]
-    ele_list[len(ele_list)-1], ele_list[orign_end_index] = ele_list[orign_end_index], ele_list[len(ele_list)-1]
-    shuffle_ele = "".join(ele_list)
-    return shuffle_ele
-"""
 
 
 def element_shuffle(ele):
