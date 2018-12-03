2cc10bdc2d7b83c50769e09d4e07ca9fc1fd42e2
yoshinGO <kamemygenki0124@gmail.com>
2018-12-03 20:16:36 +0900

    n18 done

--- chapter02/n18.py
+++ chapter02/n18.py
@@ -3,8 +3,16 @@
 確認にはsortコマンドを用いよ（この問題はコマンドで実行した時の結果と合わなくてもよい）
 """
 from sys import argv
-
+from operator import itemgetter
 
 if __name__ == '__main__':
-    with open(argv[1], 'r') as f:
-        
+    with open(argv[1], 'r') as f, open(argv[2], 'w') as g:
+        # 整列したいデータをリストに格納する
+        elements = f.readlines()
+        # 3コラム目の数値でsortedするために二次元のリストにする
+        list_elemets = [ele.split() for ele in elements]
+        upper_elements = sorted(list_elemets, key=itemgetter(2))
+        for line in upper_elements:
+            print(line)
+
+# sort -k 3 -t ' ' ../data/highttemp.txt
