0460c9d5d2a992a16e0e78d2041a1164f1bf9013
yoshinGO <kamemygenki0124@gmail.com>
2018-12-06 14:07:37 +0900

    n16 modified

--- chapter02/n16.py
+++ chapter02/n16.py
@@ -5,7 +5,7 @@
 from sys import argv
 import math
 
-split_size = int(argv[1])
+split_size = int(argv[1])  # 分割ファイルの個数
 
 with open(argv[2], 'r') as f:
     # １行ごとにリストに格納
@@ -13,14 +13,13 @@ with open(argv[2], 'r') as f:
     # リストの要素数 (行数)
     lines_length = len(lines)
     # 均等に分割するために分割ファイル一つあたりの行数を求める
-    lines_length_per_file = math.ceil(lines_length/split_size)
-    # ファイルの数
-    file_num = lines_length//lines_length_per_file
-    j = 0
-    for i in range(file_num):
-        with open(argv[3]+str(i)+'.txt', 'w')as g:
-            file = lines[j:j+lines_length_per_file]
+    lines_length_per_file = math.ceil(lines_length/split_size)  # 切り上げ
+
+    line_index = 0
+    for i in range(split_size):
+        with open(argv[3]+str(i)+'.txt', 'w') as g:
+            file = lines[line_index:line_index+lines_length_per_file]
             g.writelines(file)
-            j += lines_length_per_file
+            line_index += lines_length_per_file
 
 # split -l 3 ../tmp/merge_text.txt hoge
