8b03111fbcc483cede7c8273a1667e3c76f9ef8b
yoshinGO <kamemygenki0124@gmail.com>
2018-12-06 15:19:14 +0900

    n19 modified

--- chapter02/n19.py
+++ chapter02/n19.py
@@ -3,19 +3,20 @@
 確認にはcut, uniq, sortコマンドを用いよ．
 """
 from sys import argv
+from collections import defaultdict
 
 
 with open(argv[1]) as f:
-    data_of_list = [line.strip().split() for line in f]
-    col_1_elemtents = [col[0] for col in data_of_list]
-    count_col1 = {}
-    for ele in col_1_elemtents:
-        if ele in count_col1:
-            count_col1[ele] += 1
-        else:
-            count_col1[ele] = 1
+    # 二次元のリストが生成する
+    data_list = [line.strip().split() for line in f]
+    # data_listは複数のリストを保持するリストであり、for文を使って一つのリストを取り出す。
+    # 取り出したリストの1コラム目が必要であるからdata[0]で取得する
+    col_1_elements = [data[0] for data in data_list]
+    count_col1 = defaultdict(lambda: 0)  # lキーが存在していない時は0を返す
+    for ele in col_1_elements:
+        count_col1[ele] += 1
 
     for k, v in sorted(count_col1.items(), key=lambda x: -x[1]):
-        print(str(k) + ": " + str(v))
+        print(f'{str(k)} : {str(v)}')
 
 # cut -f 1 ../data/highttemp.txt | sort | uniq -c | sort -k1 -r
