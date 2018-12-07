2f48550392a7ea67ff3b130c0b6143ef7fe2f432
yoshinGO <kamemygenki0124@gmail.com>
2018-12-08 07:54:19 +0900

    n19 modified

--- chapter02/n19.py
+++ chapter02/n19.py
@@ -7,13 +7,11 @@ from collections import defaultdict
 
 
 with open(argv[1]) as f:
-    # 二次元のリストが生成する
-    data_list = [line.strip().split() for line in f]
-    # data_listは複数のリストを保持するリストであり、for文を使って一つのリストを取り出す。
-    # 取り出したリストの1コラム目が必要であるからdata[0]で取得する
-    col_1_elements = [data[0] for data in data_list]
+    # 取り出した行をリスト化し, その1コラム目が必要であるから[0]で取得する
+    data_list = [line.strip().split()[0] for line in f]
+
     count_col1 = defaultdict(lambda: 0)  # lキーが存在していない時は0を返す
-    for ele in col_1_elements:
+    for ele in data_list:
         count_col1[ele] += 1
 
     for k, v in sorted(count_col1.items(), key=lambda x: -x[1]):
