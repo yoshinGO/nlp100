285be107d25e06b611c4230c22c3c0bcc857bc11
yoshinGO <kamemygenki0124@gmail.com>
2018-12-19 17:53:02 +0900

    character code error

--- chapter03/n22.py
+++ chapter03/n22.py
@@ -2,13 +2,14 @@
 記事のカテゴリ名を（行単位ではなく名前で）抽出せよ
 """
 import re
+import function
 
 # 例：[[Category:G8加盟国]]
 # 例：[[Category:島国|くれいとふりてん]]
 pattern = re.compile(r'^\[\[Category:(.+?)(?:\|.+)?\]\]$')
 
-with open('../tmp/Britain.txt', 'r', encoding='utf-8') as britain_file:
-    for line in britain_file:
-        match = pattern.search(line)
-        if match:
-            print(match.group(1))
+britain_file = function.extract_UK.extract_UK('../data/jawiki-country.json.gz')
+for line in britain_file:
+    match = pattern.search(line)
+    if match:
+        print(match.group(1))
