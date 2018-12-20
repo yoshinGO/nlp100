30c6024d073e8f1509845f64839b51595d9b87a8
yoshinGO <kamemygenki0124@gmail.com>
2018-12-20 10:43:29 +0900

    n22 modified

--- chapter03/n22.py
+++ chapter03/n22.py
@@ -2,13 +2,13 @@
 記事のカテゴリ名を（行単位ではなく名前で）抽出せよ
 """
 import re
-import function
+from utils import extraction
 
 # 例：[[Category:G8加盟国]]
 # 例：[[Category:島国|くれいとふりてん]]
 pattern = re.compile(r'^\[\[Category:(.+?)(?:\|.+)?\]\]$')
 
-britain_file = function.extract_UK.extract_UK('../data/jawiki-country.json.gz')
+britain_file = extraction.extract_UK('../data/jawiki-country.json.gz')
 for line in britain_file:
     match = pattern.search(line)
     if match:
--- chapter03/utils/extraction.py
+++ chapter03/utils/extraction.py
@@ -1,3 +1,7 @@
+import gzip
+import json
+
+
 def extract_UK(fname):
     '''イギリスの関する記事本文を取得
 
@@ -5,7 +9,7 @@ def extract_UK(fname):
     イギリスの記事本文
     '''
 
-    with open(fname, 'rt') as data_file:
+    with gzip.open(fname, mode='rt', encoding='utf-8') as data_file:
         for line in data_file:
             # json形式の文字列を辞書型に変関するためにjsonモジュールのloads関数を使う
             data_json = json.loads(line)
