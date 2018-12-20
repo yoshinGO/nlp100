b23789fa65fd826a1563f13f7f0663964acd4f33
yoshinGO <kamemygenki0124@gmail.com>
2018-12-20 22:55:20 +0900

    n01 modified

--- chapter01/n01.py
+++ chapter01/n01.py
@@ -1,7 +1,9 @@
-# 「パタトクカシーー」という文字列の1,3,5,7文字目を取り出して連結した文字列を得よ．
+"""
+「パタトクカシーー」という文字列の1,3,5,7文字目を取り出して連結した文字列を得よ．
+"""
 
 seq = 'パタトクカシーー'
 odd_number_seq = seq[::2]
 
-print('seq = ' + seq)
-print('odd_number_seq = ' + odd_number_seq)
+print(f'seq = {seq}')
+print(f'odd_number_seq = {odd_number_seq}')
