76b8ccde5431e5d7452c3f48928bbdf08d687b36
yoshinGO <kamemygenki0124@gmail.com>
2018-12-20 22:49:25 +0900

    n06 modified

--- chapter01/n06.py
+++ chapter01/n06.py
@@ -1,13 +1,15 @@
 """
 'paraparaparadise'と'paragraph'に含まれる文字bi-gramの集合を，それぞれ, XとYとして求め，
 XとYの和集合，積集合，差集合を求めよ．
-さらに，'se'というbi-gramがXおよびYに含まれるかどうかを調べよ．
+さらに，'se'というbi-gramがXおよびYに含まれるかどうかを調べよ
 """
 
 
-# 文字n-gramをセット型で生成
-def make_n_gram(seq, n):
-    return set((seq[i:i + n]) for i in range(len(seq) - n + 1))
+#  n-gramをセット型で生成
+def make_n_gram(seq, n, unit):
+    if unit == 'word':
+        seq = seq.split()
+    return set((seq[i:(i+n)]) for i in range(len(seq) - n + 1))
 
 
 # 和集合を求める
@@ -25,17 +27,16 @@ def difference_two_set(x, y):
     return x - y
 
 
-# main関数
-if __name__ == "__main__":
+if __name__ == '__main__':
     seq1 = 'paraparaparadise'
     seq2 = 'paragraph'
-    # seq1, seq2それぞれの文字bi-gramを生成
-    X = make_n_gram(seq1, 2)
-    Y = make_n_gram(seq2, 2)
-    print('X : ', X)
-    print('Y : ', Y)
-    print('XとYの和集合 : ', union_two_set(X, Y))
-    print('XとYの積集合 : ', intersection_two_set(X, Y))
-    print('XとYの差集合 : ', difference_two_set(X, Y))
-    print('Xにseは含まれているか : ', 'se' in X)
-    print('Yにseは含まれているか : ', 'se' in Y)
+    # seq1, seq2のそれぞれの文字bi-gramを生成
+    X = make_n_gram(seq1, 2, 'char')
+    Y = make_n_gram(seq2, 2, 'char')
+    print(f'X：{X}')
+    print(f'Y：{Y}')
+    print(f'XとYの和集合：{union_two_set(X, Y)}')
+    print(f'XとYの積集合：{intersection_two_set(X, Y)}')
+    print(f'XとYの差集合：{difference_two_set(X, Y)}')
+    print('Xにseは含まれているか：', 'se' in X)
+    print('Yにseは含まれているか：', 'se' in Y)
