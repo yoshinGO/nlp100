d182cce9107f1b9521407d14206e4cb7498a04e6
yoshinGO <kamemygenki0124@gmail.com>
2019-01-05 15:50:37 +0900

    n40 n41

---
+++ chapter05/n40.py
@@ -0,0 +1,63 @@
+"""
+形態素を表すクラスMorphを実装せよ
+このクラスは表層形（surface），基本形（base），品詞（pos），品詞細分類1（pos1）をメンバ変数に持つこととする．
+さらに，CaboChaの解析結果（neko.txt.cabocha）を読み込み，各文をMorphオブジェクトのリストとして表現し，3文目の形態素列を表示せよ．
+"""
+from constants import FNAME_PARSED
+
+
+class Morph:
+    '''
+    形態素クラス
+    表層系(surface), 基本形(base), 品詞(pos), 品詞細分類(pos1)を
+    メンバー変数に持つ
+    '''
+    def __init__(self, surface, base, pos, pos1):
+        '''初期化'''
+        self.surface = surface
+        self.base = base
+        self.pos = pos
+        self.pos1 = pos1
+
+    def __str__(self):
+        '''オブジェクトの文字列表現'''
+        return f'surface[{self.surface}]\tbase[{self.base}]\tpos[{self.pos}]\tpos1[{self.pos1}]'
+
+
+def neko_morphs(fname_parsed):
+    with open(fname_parsed, 'r') as file_parsed:
+        morphs = []
+        for line in file_parsed:
+
+            # 1文の終了判定
+            if line == 'EOS\n':
+                yield morphs  # 1文の形態素解析結果が集まり次第yieldする。
+                morphs = []
+
+            else:
+                # 先頭が*の行は係り受け解析結果なのでスキップ
+                if line[0] == '*':
+                    continue
+
+                # 表層系はtab区切り, それ以外は','で区切りバラす
+                cols = line.split('\t')  # cols[0]は表層系, cols[1]は解析結果
+                res_cols = cols[1].split(',')
+
+                # Morph作成, リストに追加
+                morphs.append(Morph(
+                    cols[0],  # surface
+                    res_cols[6],  # base
+                    res_cols[0],  # pos
+                    res_cols[1],  # pos1
+                ))
+
+        raise StopIteration
+
+
+if __name__ == '__main__':
+    for i, morphs in enumerate(neko_morphs(FNAME_PARSED), 1):  # neko_lines(FNAME_PARSED)はgenerator
+        # 3文目を表示
+        if i == 3:  # 1ベースのi, 最初2回分のEOSは意味のないものであるから挙動を確認するためにi==3の時を確認
+            for morph in morphs:
+                print(morph)
+            break
---
+++ chapter05/n41.py
@@ -0,0 +1,116 @@
+"""
+40に加えて，文節を表すクラスChunkを実装せよ.
+このクラスは形態素（Morphオブジェクト）のリスト（morphs），係り先文節インデックス番号（dst），
+係り元文節インデックス番号のリスト（srcs）をメンバ変数に持つこととする．
+さらに，入力テキストのCaboChaの解析結果を読み込み，１文をChunkオブジェクトのリストとして表現し，
+8文目の文節の文字列と係り先を表示せよ．
+第5章の残りの問題では，ここで作ったプログラムを活用せよ
+"""
+import re
+from constants import FNAME_PARSED
+from n40 import Morph
+
+
+class Chunk:
+    '''
+    文節クラス
+    形態素(Morphオブジェクト)のリスト(morphs), 係り先文節インデックス番号(dst),
+    係り元文節インデックス番号のリスト(srcs)をメンバー変数に持つ
+    '''
+
+    def __init__(self):
+        '''初期化'''
+        self.morphs = []  # 1文の形態素解析結果が格納されているリスト
+        self.srcs = []
+        self.dst = -1
+
+    def __str__(self):
+        '''オブジェクトの文字列表現'''
+        surface = ''
+        '''
+        morphsには一文節『吾輩は』の形態素解析結果の
+        『吾輩	名詞,代名詞,一般,*,*,*,吾輩,ワガハイ,ワガハイ,,』と
+        『は	助詞,係助詞,*,*,*,*,は,ハ,ワ,,』が入っている
+        '''
+        for morph in self.morphs:
+            surface += morph.surface  # 『吾輩』と『は』を足し合わせて『吾輩は』という一文節が完成する.
+        return f'{surface}\tsrcs{self.srcs}\tdst[{self.dst}]'  # [0]吾輩は	srcs[]	dst[5]
+
+
+def neko_chunk(fname_parsed):
+    '''「吾輩は猫である」の係り受け解析結果のジェネレータ
+    「吾輩は猫である」の係り受け解析結果を順次読み込んで,
+    1文ずつChunkクラスのリストを返す
+
+    戻り値：
+    1文のChunkクラスのリスト
+    '''
+    with open(fname_parsed) as file_parsed:
+
+        chunks = dict()  # idxをkeyにChunkを格納
+        idx = -1
+
+        for line in file_parsed:
+
+            # 1文の終了判定
+            if line == 'EOS\n':
+
+                # Chunkのリストを返す
+                if len(chunks) > 0:
+
+                    # chunksをkeyでソートし, valueのみ取り出し
+                    # xを与えるとx[0]を返すためkeyでのソートとなる
+                    sorted_tuple = sorted(chunks.items(), key=lambda x: x[0])  # sorted関数の返り値はリスト
+                    yield list(zip(*sorted_tuple))[1]  # 文が終わり次第, valueのみを取り出しyieldする.
+                    chunks.clear()
+
+                else:
+                    yield []
+            # 先頭が*の行は係り受け解析結果なので, Chunkを作成. 文節ごとに*が存在する
+            elif line[0] == '*':
+
+                # Chunkのインデックス番号と係り先のインデックス番号取得
+                cols = line.split(' ')  # 『* 0 2D 0/0 -0.764522』 この形式の行をスペース区切りでリスト化する
+                idx = int(cols[1])  # 何番目の文節かの情報が格納されている
+                dst = int(re.search(r'(.*?)D', cols[2]).group(1))  # 係り先の文節の番号
+
+                # Chunkを生成(なければ)し, 係り先のインデックス番号セット
+                if idx not in chunks:
+                    chunks[idx] = Chunk()
+                chunks[idx].dst = dst
+
+                # 係り元のChunkを生成(なければ)そ, 係り元インデックス番号追加
+                if dst != -1:
+                    if dst not in chunks:
+                        chunks[dst] = Chunk()
+                    chunks[dst].srcs.append(idx)  # chunksオブジェクトのsrcsの型はリスト
+
+            # それ以外の行は形態素解析結果なので, Morphを作りChunkに追加
+            else:
+
+                # 表層系はtab区切り, それ以外は','区切りでバラす
+                # 『吾輩	名詞,代名詞,一般,*,*,*,吾輩,ワガハイ,ワガハイ,,』
+                cols = line.split('\t')  # [吾輩,その他の情報がカンマ区切りで格納されたもの]という形式になる
+                res_cols = cols[1].split(',')
+
+                # Morph作成, リストに追加
+                chunks[idx].morphs.append(
+                    Morph(
+                        cols[0],  # surface
+                        res_cols[6],  # base
+                        res_cols[0],  # pos
+                        res_cols[1],  # pos1
+                    )
+                )
+        raise StopIteration
+
+
+if __name__ == '__main__':
+    # 1文ずつリスト作成
+    for i, chunks in enumerate(neko_chunk(FNAME_PARSED), 1):
+
+        # 8文目を表示
+        if i == 8:
+            for j, chunk in enumerate(chunks):
+                print(f'[{j}]{chunk}')
+            break
